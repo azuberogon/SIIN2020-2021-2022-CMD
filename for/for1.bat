a814be76b8df5dac6963184afcf346c0a2999698
azuberogon <azuberogondam1b@gmail.com>
2022-02-10 20:55:55 +0100

    CONFIRMAR

---
+++ for/for1.bat
@@ -0,0 +1,7 @@
+@echo off
+::en este codigo se hara un bucle 
+
+
+for %%A in (%*) do (
+    echo argumento:%%A
+)
---
+++ for/for2.bat
@@ -0,0 +1,5 @@
+@echo off
+
+for %%A in ("uno" "dos" "tres" "cuatro" "cinco" "seis")(
+    echo %%A
+)

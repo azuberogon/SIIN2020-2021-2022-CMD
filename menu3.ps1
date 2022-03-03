214888f1d655d5c6fdf8d700c6883e002614d80a
azuberogon <azuberogondam1b@gmail.com>
2022-03-03 20:55:54 +0100

    Create menu3.ps1

---
+++ menu3.ps1
@@ -0,0 +1,6 @@
+"1. Pedir un alias y mostrar su cmdlet
+2. Pedir un cmdlet y motrar todos sus alias
+3. Pedir un cmdlet y mostar su ayuda
+4. Pedir un texto y mostrar todos los verbos que empiecen por el texto
+5. Pedir un texto y mostrar lodos los cmdlet que empiecen por el texto
+S. Salir"
